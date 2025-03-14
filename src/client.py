f7f4f7621ebc0c544fc8db3934a997b9b2e69c50
Magius Pendragon <magius@faerlin.magiuspendragon.yi.org>
2025-03-14 17:23:33 -0600

    Added chat history to the client

--- src/client.py
+++ src/client.py
@@ -1,28 +1,28 @@
 from pathlib import Path
-from typing import TypedDict
+from typing import TypedDict, Annotated
 import os
 
 from dotenv import load_dotenv
 from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_ollama import ChatOllama
 from langgraph.graph import START, StateGraph
+from langgraph.graph.message import add_messages
 
 from data import DocumentStore, PDFDocumentStore
 
 class ChatClient:
 
     class State(TypedDict):
-        question: str
+        messages: Annotated[list[HumanMessage | AIMessage], add_messages]
         context: list[Document]
-        answer: str
-
 
     def __init__(self, model, store):
         self._model = model
         self._store = store
         self._init_llm()
-
+    
     def _init_llm(self):
         self._llm =  ChatOllama(model=self._model) # check if we want other parameters
         self._prompt = ChatPromptTemplate([
@@ -37,18 +37,29 @@ class ChatClient:
         self._graph = graph_builder.compile()
 
     def retrieve(self, state : State) -> State:
-        retrieved_docs = self._store.query(state['question'], None)
-        return {'context' : retrieved_docs}
+        question = state["messages"][-1] if state["messages"] else AIMessage(content="Welcome, how can I help you today?")
+        match question:
+            case AIMessage():
+                return state
+            case HumanMessage():
+                retrieved_docs = self._store.query(question.content, None)
+                return self.State(messages=state["messages"], context=retrieved_docs)
+            case _:
+                return state # Not sure what message we received, do nothing
 
     def generate(self, state : State) -> State:
         doc_content = '\n\n'.join(doc.page_content for doc in state['context'])
-        messages = self._prompt.invoke({'question' : state['question'], 'context' : doc_content})
+        question = state["messages"][-1] if state["messages"] else AIMessage(content="Welcome, how can I help you today?")
+        messages = self._prompt.invoke({'question' : question.content, 'context' : doc_content})
         response = self._llm.invoke(messages)
 
-        return {'answer' : response.content}
+        return self.State(
+                messages = state["messages"] + [AIMessage(content=response.content)],
+                context=state["context"]
+        )
 
     def ask(self, question : str) -> State:
-        return self._graph.invoke({"question" : question})
+        return self._graph.invoke(self.State(messages=[HumanMessage(content=question)]))
 
 
 
@@ -70,5 +81,5 @@ if __name__ == "__main__":
     question = input("Ask a question about the paper:\n>> ") #"What is crystal island?"
     result = client.ask(question)
     print(f"There are {len(result['context'])} documents in the context.")
-    print(f"Answer: {result['answer']}")
+    print(f"Answer: {result['messages'][-1].content}")
 
